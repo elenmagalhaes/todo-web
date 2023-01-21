a9ca8b3a2019ae4e8ff66a97196c9c86bdd602ac
Elen Magalhães <fromeleen@gmail.com>
2023-01-21 11:52:37 -0300

    armazenando nova tarefa no state

--- src/components/Task/NewTask/NewTask.tsx
+++ src/components/Task/NewTask/NewTask.tsx
@@ -1,16 +1,34 @@
-import './NewTask.scss'
 import { PlusCircle } from 'phosphor-react'
+import { useState, ChangeEvent, FormEvent } from 'react'
+import './NewTask.scss'
 
 export function NewTask() {
+  const [tasks, setTasks] = useState<string[]>([])
+  const [newTask, setNewTask] = useState<string>('')
+
+  const handleCreateTask = (event: FormEvent) => {
+    event.preventDefault()
+    setTasks([...tasks, newTask])
+  }
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setNewTask(event.target.value)
+  }
+
   return (
-    <div className="new-task-container">
-      <input placeholder="Adicone uma nova tarefa" type="text" name="tarefa" />
+    <form onSubmit={handleCreateTask} className="new-task-container">
+      <input
+        placeholder="Adicone uma nova tarefa"
+        name="tarefa"
+        type="text"
+        onChange={handleInputChange}
+      />
       <button type="submit">
         <div className="btn-content">
           <span>Criar</span>
           <PlusCircle size={16} color="#FFFFFF" weight="bold" />
         </div>
       </button>
-    </div>
+    </form>
   )
 }
