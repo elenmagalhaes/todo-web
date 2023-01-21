a1e3d1a3f79a70dd24c2e6ba7e078a95db6d61ed
Elen Magalhães <fromeleen@gmail.com>
2023-01-21 10:18:14 -0300

    iniciado desenvolvimento da lista de tarefas

---
+++ src/components/EmptyState/EmptyState.tsx
@@ -0,0 +1,5 @@
+import './EmptyState.scss'
+
+export function EmptyState() {
+  return <div>EmptyState</div>
+}
---
+++ src/components/Task/TaskList/TaskList.tsx
@@ -0,0 +1,20 @@
+import './TaskList.scss'
+
+export function TaskList() {
+  return (
+    <div className="task-list-container">
+      <header className="header-info">
+        <div className="created-tasks">
+          <span>Tarefas criadas</span>
+          <span className="counter">0</span>
+        </div>
+        <div className="completed-tasks">
+          <span>Concluídas</span>
+          <span className="progress">1 de 5</span>
+        </div>
+      </header>
+
+      <div></div>
+    </div>
+  )
+}
