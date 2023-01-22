0b2f3d8e0a083a476b510e767f0d913b1abe6d15
Elen Magalhães <fromeleen@gmail.com>
2023-01-21 22:43:25 -0300

    ajustado a origem dos assets da aplicação

--- src/components/EmptyState/EmptyState.tsx
+++ src/components/EmptyState/EmptyState.tsx
@@ -3,7 +3,7 @@ import './EmptyState.scss'
 export function EmptyState() {
   return (
     <div className="empty-state-container">
-      <img src="./src/assets/clipboard.svg" alt="" />
+      <img src="/clipboard.svg" alt="" />
       <div className="container-info">
         <p className="title">Você ainda não tem tarefas cadastradas</p>
         <p className="description">
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import './Header.scss'
 export function Header() {
   return (
     <header className="header">
-      <img src="./src/assets/rocket.svg" alt="logo de foguete" />
+      <img src="/rocket.svg" alt="logo de foguete" />
     </header>
   )
 }
